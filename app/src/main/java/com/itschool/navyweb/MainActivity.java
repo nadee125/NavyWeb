94c9391db81911c3ca1745b74b67057195264066
nadee125 <nadeerathnayake8@gmail.com>
2023-12-22 12:03:29 -0800

    Initialization
    Create app for view Navy.lk

---
+++ app/src/main/java/com/itschool/navyweb/MainActivity.java
@@ -0,0 +1,48 @@
+package com.itschool.navyweb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mywebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mywebView=(WebView) findViewById(R.id.webview);
+        mywebView.setWebViewClient(new WebViewClient());
+        mywebView.loadUrl("https://www.navy.lk ");
+        WebSettings webSettings=mywebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+                // on below line displaying a log that admob ads has been initialized.
+                Log.i("Admob", "Admob Initialized.");
+            }
+        });
+
+        // on below line creating and initializing variable for adView.
+        AdView adView = findViewById(R.id.adView);
+        // on below line creating and initializing variable for adRequest
+        AdRequest adRequest = new AdRequest.Builder().build();
+        // on below line loading request inside our adview.
+        adView.loadAd(adRequest);
+
+    }
+}
